3f4d486b8ebba5877ce0cb8133f59c9c966a3655
Shankun Lin <lin.mhshs@gmail.com>
2017-01-03 03:14:33 -0500

    Basic modal complete

--- public/javascripts/coplayer.js
+++ public/javascripts/coplayer.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  setUpModal();
+  CoPlayer();
   // adjustFloatingActions();
   // $(document).scroll(adjustFloatingActions);
   // $(window).resize(adjustFloatingActions);
@@ -18,3 +20,24 @@ $(document).ready(function(){
 //     }
 //   }
 // }
+
+function CoPlayer() {
+  $('form.coplayer-search').submit(function(){
+    // SEARCH EVENT GO HERE
+    event.preventDefault();
+  });
+}
+
+function setUpModal() {
+  $('.modal-holder').find('.btn-close').click(function(){
+    $(this).closest('.modal-holder').removeClass('show');
+  });
+  $('.modal-holder').bind('click.closeModal', function(e){
+    if(!$(e.target).closest('.modal-box').length){
+      $(this).removeClass('show');
+    };
+  });
+  $('.coplayer-btn-add').bind('click.showSearch',function(e){
+    $('#coplayer-search-modal.modal-holder').addClass('show');
+  });
+}
